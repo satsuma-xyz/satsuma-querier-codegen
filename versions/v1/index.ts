7e55d416610f15aa7691e1989f541e554238eace
Andrew Webb <androbwebb@gmail.com>
2023-05-12 01:49:10 +0100

    Update the pluginLoader

--- versions/v1/index.ts
+++ versions/v1/index.ts
@@ -7,16 +7,16 @@ import * as path from "path";
 import {printSchema} from "graphql";
 import type {CodegenConfig} from "@graphql-codegen/cli";
 import {executeCodegen} from "@graphql-codegen/cli";
-import {plugin as tsPlugin} from "../../shared/gql-ts/src/index";
 
 const gqlCodegenConfig = (schemaPath: string, outputPath: string): CodegenConfig => ({
     overwrite: true,
     schema: schemaPath,
-
-    //@ts-ignore
+    pluginLoader: async (name: string) => {
+        return await import(name);
+    },
     generates: {
         [outputPath]: {
-            plugins: [tsPlugin],
+            plugins: ["typescript"],
         },
     },
 })
